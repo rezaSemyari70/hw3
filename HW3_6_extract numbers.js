44e1e1c82efc79627747bcc3e82919f22821f934
reza <rezasemyari1991@gmail.com>
2020-04-16 12:38:08 +0430

    edit hw3_6

--- HW3_6_extract numbers.js
+++ HW3_6_extract numbers.js
@@ -1,6 +1,6 @@
 // function with array methods
 function extractNumbers(str){
-    let res = str.split('').filter(item => +item).map(item => Number(item)) ;
+    let res = str.split('').filter(item => +item || +item === 0).map(item => Number(item)) ;
     return res ;
 }
 
@@ -12,7 +12,7 @@ console.log(extractNumbers("ihave2applesand3pineapples")) ;
 function algorithemExtractNumbers(str){
     let res = [] , j = 0;
     for(let i = 0; i < str.length; i++){
-        if( +str.charAt(i) )  
+        if( +str.charAt(i) || +str.charAt(i) === 0)  
             res[j++] = +str.charAt(i) ;
     }
     return res ; 
